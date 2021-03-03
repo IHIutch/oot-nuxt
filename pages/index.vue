de51ea1be1de6fb2da95af04120a8c1f0dfea15e
IHIutch <jbhutch01@gmail.com>
2021-03-02 19:30:50 -0500

    Create pages, install sass, sass-loader@10

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,143 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">test</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <div class="bg-light py-5">
+      <b-container>
+        <b-row>
+          <b-col class="text-center">
+            <div class="mb-5">
+              <h1 class="display-2">Open Office Time</h1>
+            </div>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="6">
+            <p class="h2">
+              Schedule time with community leaders, mentors, executives, and
+              advisors.
+            </p>
+          </b-col>
+          <b-col cols="6">
+            <p class="lead mb-4">
+              A generous listing of everyone who has open office time available
+              in Buffalo, Rochester, Upstate NY, and more! Get advice, feedback,
+              or workshop your business ideas.
+            </p>
+            <div>
+              <b-button href="#content" variant="primary" class="mb-2"
+                >Search the Listings</b-button
+              >
+            </div>
+            <div>
+              <b-button to="/join" variant="link" class="px-0"
+                >Add Your Open Office Time</b-button
+              >
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+    <div class="position-relative pt-3">
+      <div class="py-4 sticky-top bg-white">
+        <b-container>
+          <b-row>
+            <b-col>
+              <b-form-group
+                id="content"
+                label="Search"
+                label-for="search"
+                label-class="sr-only"
+                class="mb-0"
+              >
+                <b-form-input
+                  id="search"
+                  v-model="form.search"
+                  type="text"
+                  placeholder="Search & filter..."
+                  size="lg"
+                ></b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-row>
+        </b-container>
+      </div>
+      <div class="mb-5 pt-3">
+        <b-container>
+          <b-row>
+            <b-col v-for="(a, idx) in advisors" :key="idx" cols="3">
+              <div class="rounded shadow bg-white p-3 h-100 d-flex flex-column">
+                <div class="text-center flex-grow-1">
+                  <div class="">
+                    <div class="mb-3">
+                      <img
+                        class="h-20 w-20 rounded-circle object-cover"
+                        :src="a.imageURL"
+                        :alt="a.name"
+                      />
+                    </div>
+                    <h2 class="font-weight-bold h3">
+                      {{ a.firstName }} {{ a.lastName }}
+                    </h2>
+                    <p class="mb-3">{{ a.description }}</p>
+                  </div>
+                </div>
+                <b-button
+                  :href="a.buttonURL"
+                  target="_blank"
+                  no-referrer
+                  no-openner
+                  >Book Now</b-button
+                >
+              </div>
+            </b-col>
+          </b-row>
+        </b-container>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      form: {
+        search: '',
+      },
+      advisors: [
+        {
+          firstName: 'Jonathan',
+          lastName: 'Gorczyca',
+          description:
+            'Partner / Co-founder at Helm Experience & Design. Product Design, UX, Technology, Startups, Digital Strategy.',
+          buttonURL: 'https://calendly.com/jonathan-gorczyca/connect',
+          imageURL: 'https://www.openofficetime.com/assets/img/heads/jg.jpg',
+          tags: ['ux design', 'product design', 'startups', 'technology'],
+        },
+        {
+          firstName: 'Nicholas',
+          lastName: 'Barone',
+          description:
+            'I have spent my entire career working to increase the number of technology companies and entrepreneurs in Buffalo, NY',
+          buttonURL: 'https://calendly.com/nb3004',
+          imageURL:
+            'https://www.openofficetime.com/assets/img/heads/82311f376f5d8a12.jpg',
+          tags: ['entrepreneur', 'startups', 'technology'],
+        },
+      ],
+    }
+  },
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style lang="scss" scoped>
+.h-20 {
+  height: 5rem;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.w-20 {
+  width: 5rem;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.object-cover {
+  object-fit: cover;
 }
 </style>
---
+++ pages/join.vue
@@ -0,0 +1,179 @@
+<template>
+  <div>
+    <div class="bg-light py-5">
+      <b-container>
+        <b-row>
+          <b-col class="text-center">
+            <h1 class="display-2">
+              <NuxtLink to="/">Open Office Time</NuxtLink>
+            </h1>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+    <div class="py-5">
+      <b-container>
+        <b-row>
+          <b-col cols="6" offset="3">
+            <div>
+              <b-form @submit.prevent="onSubmit">
+                <b-form-group
+                  label="What's your first name?"
+                  label-for="firstName"
+                  description=""
+                >
+                  <b-form-input
+                    id="firstName"
+                    v-model="form.firstName"
+                    type="text"
+                    placeholder="Type here..."
+                    required
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                  label="What's your last name?"
+                  label-for="lastName"
+                  description=""
+                >
+                  <b-form-input
+                    id="lastName"
+                    v-model="form.lastName"
+                    type="text"
+                    placeholder="Type here..."
+                    required
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                  label="How about your email?"
+                  label-for="email"
+                  description=""
+                >
+                  <b-form-input
+                    id="email"
+                    v-model="form.email"
+                    type="email"
+                    placeholder="Your work email..."
+                    required
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                  label="What city are you in?"
+                  label-for="city"
+                  description=""
+                >
+                  <b-form-input
+                    id="city"
+                    v-model="form.city"
+                    type="text"
+                    placeholder="eg. Buffalo..."
+                    required
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                  label="Give us an overview of your expertise"
+                  label-for="description"
+                  description="We'll display this overview on your listing. Please keep it short and sweet. (140 characters max.)"
+                >
+                  <b-form-input
+                    id="description"
+                    v-model="form.description"
+                    type="text"
+                    placeholder="Clear and succinct..."
+                    maxlength="140"
+                    required
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                  label="Share your LinkedIn"
+                  label-for="linkedinURL"
+                  description="Find your profile link by going to your profile and copy / paste the url."
+                >
+                  <b-form-input
+                    id="linkedinURL"
+                    v-model="form.linkedinURL"
+                    type="url"
+                    placeholder="Paste your profile link here..."
+                    required
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                  label="Share the link to your booking calendar"
+                  label-for="bookingURL"
+                  description="Most prefer a Calendly to manage their hours and booking."
+                >
+                  <b-form-input
+                    id="bookingURL"
+                    v-model="form.bookingURL"
+                    type="url"
+                    placeholder="Paste your Calendly link..."
+                    required
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                  v-slot="{ ariaDescribedby }"
+                  label="Select the top 3 categories you mentor"
+                  description="This will help people to find your profile by category. (Max. 3)"
+                >
+                  <b-form-checkbox-group
+                    id="categories"
+                    v-model="form.categories"
+                    class="text-capitalize"
+                    :options="options"
+                    :aria-describedby="ariaDescribedby"
+                    name="categories"
+                    stacked
+                  ></b-form-checkbox-group>
+                </b-form-group>
+
+                <div class="text-center">
+                  <b-button class="mt-3" type="submit" variant="primary"
+                    >Submit Your Listing</b-button
+                  >
+                </div>
+              </b-form>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JoinPage',
+  data() {
+    return {
+      options: [
+        'ux design',
+        'product design',
+        'startups',
+        'technology',
+        'entrepreneur',
+      ],
+      form: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        city: '',
+        description: '',
+        linkedinURL: '',
+        bookingURL: '',
+        categories: [],
+      },
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.form)
+    },
+  },
+}
+</script>
