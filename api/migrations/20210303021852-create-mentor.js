35aeabbddee46c9cda449f341430482bdb940aac
IHIutch <jbhutch01@gmail.com>
2021-03-02 21:22:05 -0500

    Generate Mentor model

---
+++ api/migrations/20210303021852-create-mentor.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Mentors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      linkedinURL: {
+        type: Sequelize.STRING
+      },
+      bookingURL: {
+        type: Sequelize.STRING
+      },
+      categories: {
+        type: Sequelize.JSON
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Mentors');
+  }
+};
